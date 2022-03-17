f381d3e17d939f3d0c294e0538b45fcd4188f254
realmers <danhoalexander123@gmail.com>
2022-03-17 20:30:20 +0100

    additional config

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+interface Color {
+    red: number;
+    green: number;
+    blue: number;
+}
+
+const color: Color = {
+    red: 33,
+    green: 22,
+    blue: 11
+};
+
+console.log(color);
+
+export default color;
