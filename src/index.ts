03383e83d34c2888220649d70d8eb073df987055
realmers <danhoalexander123@gmail.com>
2022-03-17 20:55:59 +0100

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,12 @@
-interface Color {
-    red: number;
-    green: number;
-    blue: number;
-}
+export * from "./errors/bad-request-error";
+export * from "./errors/custom-error";
+export * from "./errors/database-connection-error";
+export * from "./errors/not-authorized-error";
+export * from "./errors/not-found";
+export * from "./errors/request-validation-error";
 
-const color: Color = {
-    red: 33,
-    green: 222,
-    blue: 112
-};
+export * from "./middlewares/current-user";
+export * from "./middlewares/error-handler";
+export * from "./middlewares/require-auth";
+export * from "./middlewares/validate-request";
 
-console.log(color);
-
-export default color;
